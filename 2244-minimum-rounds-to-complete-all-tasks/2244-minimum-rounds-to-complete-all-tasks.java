edf10d1995c327768af956abae95cbef6ef07c40
Akash Kant <thakur.akashkant@gmail.com>
2023-01-04 03:33:31 -0700

    Time: 104 ms (19.83%), Space: 111 MB (10.09%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.java
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.java
@@ -7,11 +7,10 @@ class Solution {
         Iterator<Integer> it = hmMap.keySet().iterator();
         int ans = 0;
         while (it.hasNext()) {
-            int key = it.next();
-            int val = hmMap.get(key);
+            int val = hmMap.get(it.next());
             if (val == 1) return -1;
-            else if (val % 3 != 0  ) ans += val / 3 + 1;
-            else ans += val / 3;
+            ans += val / 3;
+            if (val % 3 != 0) ans++;
         }
         return (int) ans;
     }
