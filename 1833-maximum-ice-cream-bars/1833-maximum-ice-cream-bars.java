c9f41567e2f1ad51bd3ff1d36557ccfef1dbf54d
Akash Kant <thakur.akashkant@gmail.com>
2023-01-05 22:12:04 -0700

    Time: 68 ms (5.69%), Space: 77 MB (28.91%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
@@ -1,18 +1,16 @@
 class Solution {
     public int maxIceCream(int[] costs, int coins) {
-      ArrayList<Integer> costVal = Arrays.stream(costs)
-                .boxed()
-                .collect(Collectors.toCollection(ArrayList::new));
-        Collections.sort(costVal);
-        int noOfBars = 0;
-        for (int i : costVal) {
-            if (coins >= i) {
-                coins -= i;
-                noOfBars++;
-            } else {
-                return noOfBars;
-            }
-        }
-        return noOfBars;  
+         Arrays.sort(costs);
+
+        // Use IntStream to filter the costs array and sum the elements
+        final int[] numCoins = new int[]{coins};
+
+        // Use IntStream to filter the costs array and count the number of elements
+        int numIceCreamBars = Math.toIntExact(Arrays.stream(costs)
+                .filter(cost -> cost <= numCoins[0]) // Keep only costs that can be afforded
+                .peek(cost -> numCoins[0] -= cost) // Decrement the number of coins by the cost of the ice cream bar
+                .count()); // Count the elements of the stream
+
+        return numIceCreamBars;
     }
 }
